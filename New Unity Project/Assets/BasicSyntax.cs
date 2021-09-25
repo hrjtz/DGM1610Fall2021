3955de9929d3fb1aef00676774e9a5790c27fc03
Hide Ziegler <hidez246@gmail.com>
2021-09-24 20:01:55 -0600

    BasicSyntax added

---
+++ New Unity Project/Assets/BasicSyntax.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicSyntax : MonoBehaviour
+{
+    Debug.Log(transform.position.x);
+
+    if(transform.position.y <= 5f)
+    {
+        Debug.Log ("I'm about to hit the ground!");
+    }
+}
