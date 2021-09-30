703705a5649310593bf0ece0a733190cc61f9c20
Hide Ziegler <hidez246@gmail.com>
2021-09-29 22:40:52 -0600

    Operators added

---
+++ New Unity Project/Assets/Operators.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Operators : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int x = 2;
+        int y = 6;
+        int num1 = x + y;
+        float num2 = x / y;
+        int num3 = x % y;
+        int num4 = x * y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
