785f24e386f90d40443726144c5d168f8dfb6509
Hide Ziegler <hidez246@gmail.com>
2021-09-29 22:37:27 -0600

    Changed Variables

--- New Unity Project/Assets/Variables.cs
+++ New Unity Project/Assets/Variables.cs
@@ -8,7 +8,10 @@ public class Operations : MonoBehaviour
     void Start()
     {
         int myNum = 7;
-        char myChar
+        char myChar = 'z';
+        string myString = 'Heya dudes.';
+        bool myBool = false;
+        float myFloat = '3.1415';
     }
 
     // Update is called once per frame
